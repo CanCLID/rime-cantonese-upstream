82051d47ec54d1995b52d55762e29567dfb03930
Kevin Li <kevinli020508@gmail.com>
2022-06-05 08:36:52 -0400

    Output error messages to stdout in find_duplicates.py

--- scripts/find_duplicates.py
+++ scripts/find_duplicates.py
@@ -1,9 +1,9 @@
 """
 Finds all duplicated lines within each CSV file as well as across all files.
-It outputs a duplicates.tsv containing the duplicated lines and
-a list of two or more filenames with the line number of the duplicated lines.
+It outputs the duplicated line with a list of two or more filenames with
+line numbers of the duplicated lines.
 
-Sample line in duplicates.tsv looks like:
+A sample line in the output looks like:
 到未啊,dou3 mei6 aa3	[phrase_fragment.csv:1830, word.csv:11234]
 
 This means that the line "到未啊,dou3 mei6 aa3" is duplicated
@@ -24,11 +24,7 @@ for filename in iglob('*.csv'):
             else:
                 line_to_locations[line] = [location]
 
-f = open('duplicates.tsv', 'w')
-
 for line, locations in line_to_locations.items():
     if len(locations) > 1:
-        locations_str = ', '.join(locations)
-        f.write(f'{line.strip()}\t[{locations_str}]\n')
-
-f.close()
+        locations_str = ' and '.join(locations)
+        print(f'WARNING: {line.strip()} is duplicated in {locations_str}\n')
