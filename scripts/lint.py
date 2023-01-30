1ec195b6875f7a80269788846a18ded74948e69c
graphemecluster <graphemecluster@gmail.com>
2023-01-30 13:59:33 +0800

    Check other CSVs

--- scripts/lint.py
+++ scripts/lint.py
@@ -3,17 +3,24 @@ import time
 from glob import iglob
 from os.path import basename
 from itertools import zip_longest, accumulate
+from bisect import bisect
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-non_han = {*'，：'}
+non_han = {*'，： '}
 other_han = {*'〇﨎﨏﨑﨓﨔﨟﨡﨣﨤﨧﨨﨩'}
 multisyllable_allowlist = {*'兡瓸䇉竡尣兛瓩竏𥪕兝瓰竕嗧浬兞瓱竓呎吋啢𠺖兣糎甅竰卅𠯢兙瓧䇆竍卌'}
-other_column_values = {
+column_values = {
     "pron_rank": {"預設", "常用", "罕見", "棄用"},
     "tone_var": {"", "本調", "變調"},
     "literary_vernacular": {"", "文讀", "白讀"},
+    "class": {"罕見", "日文", "韓文", "來歷不明"},
 }
+
+simplified_start = "䌶䜣䞌䢀䥺䦶䭪䯃䲝䴓䶭丬卤纟见觇讠贝车钅长门韦页风飞饣马鱼鸟鹾麦麸黄黾鼋齐齿龙龟鿏鿔鿭鿴𦈈𧮪𧹑𨐅𨰾𨷿𩏼𩖕𩙥𩟾𩧦𩽹𩾁𩾊𩾎𪉁𪎈𪚏𫄙𫌨𫍙𫎦𫐄𫓥𫔬𫖑𫖪𫗇𫗞𫘛𫚈𫛚𫜊𫜑" + \
+    "𫜟𫜨𫜲𫟃𫟞𫟤𫟲𫠅𫠊𫠏𫠖𫠜𬌒𬘓𬢇𬣙𬥳𬨁𬬧𬮘𬰱𬱓𬱵𬲥𬳳𬶀𬷕𬷻𬸵𬹅𬹣𬹳𬹺𬺛𮉠𮙉𮝳𮣲𮤫𮧴𮨴𮩛𮪡𮬛𮭡𮭰𮮃𮯙黾𰠗𰫼𰴕𰵊𰷞𰹯𰽕𰿖𰿥𱂃𱂠𱃔𱃱𱄼𱇍𱉇𱊺𱋄𱋾𱌗𱌩𱍁𱭗𱺏𲁑𲂂𲂺𲅀𲇭𲈤𲈵𲊥𲊹𲋎𲋓𲋢𲌄𲍆𲍫𲎑𲎧𲎮"
+simplified_end = "䍁䜩䞐䢂䦆䦸䭪䯅䲤䴙䶮丬卤缵觅觑谶赣辚镶长阛韬颧飚飞馕骧鳤鹴鹾麦麺黄黾鼍齑龌龛龟鿏鿕鿭鿺𦈡𧮪𧹗𨐊𨱖𨸎𩐀𩖗𩙰𩠏𩨐𩽿𩾈𩾌𩾎𪉕𪎐𪚐𫄹𫌭𫍿𫎬𫐙𫔕𫔹𫖖𫖺𫗌𫗵𫘱𫚭𫜆𫜊𫜕" + \
+    "𫜟𫜰𫜳𫟇𫟢𫟦𫠂𫠈𫠌𫠒𫠗𫠜𬌒𬙋𬢔𬤱𬦀𬨕𬮃𬮹𬰸𬱳𬲈𬳔𬴐𬶻𬷕𬸱𬸹𬹎𬹤𬹳𬺖𬺞𮉯𮙋𮝺𮣷𮤸𮧵𮨶𮩞𮪥𮬤𮭪𮭰𮮇𮯙黾𰠘𰭁𰴞𰶏𰷮𰺤𰿊𰿖𱀀𱂌𱂻𱃠𱄊𱅬𱈜𱊵𱊽𱋮𱌉𱌙𱌽𱍈𱭘𱺰𲁙𲂗𲃆𲅃𲈡𲈥𲉈𲊥𲋃𲋑𲋓𲋬𲌋𲍙𲎊𲎓𲎫𲎮"
 
 with open('scripts/ignore.csv', encoding='utf-8') as f:
     next(f)
@@ -27,6 +34,9 @@ def is_unified_ideograph(char):
            '\U00030000' <= char <= '\U000323af' or \
            char in other_han
 
+def is_simplified_ideograph(char):
+    return "䌶" <= char <= simplified_end[bisect(simplified_start, char) - 1]
+
 def get_additional_information(char):
     return ': Radicals' if '\u2e80' <= char <= '\u2fef' else \
            ': Symbols and Punctuation' if '\u3000' <= char <= '\u303f' else \
@@ -35,8 +45,14 @@ def get_additional_information(char):
            ': Compatibility Ideographs Supplement' if '\U0002f800' <= char <= '\U0002fa1f' else \
            ''
 
+def is_ascii_letter(char):
+    return 'A' <= char <= 'Z' or 'a' <= char <= 'z'
+
 def utf16_byte_length(char):
     return (char >= '\U00010000') + 1
+
+def column_start(column):
+    return len(column) + 1
 
 cache = {}
 headers = {}
@@ -47,16 +63,18 @@ def lint(filename):
         new_messages = []
 
         first_line = next(f, "")
-        if not first_line.startswith('char,jyutping'):
-            new_messages.append(f'{filename}:1:1,{sum(map(utf16_byte_length, first_line)) + 1}: ERROR: Invalid CSV header')
-        other_column_names = first_line.rstrip('\n').split(',')[2:]
+        column_names = first_line.rstrip('\n').split(',')
+        chars_column_index = column_names.index("char") if "char" in column_names else None
+        romans_column_index = column_names.index("jyutping") if "jyutping" in column_names else None
+        start_column_index = min(chars_column_index or 0, romans_column_index or 0)
+        end_column_index = max(chars_column_index or 0, romans_column_index or 0)
 
         if first_line != headers.get(filename):
             cache[filename] = {}
 
         for line_num, line in enumerate(f, 2):
             if line in cache[filename]:
-                new_messages += map(lambda line: f'{filename}:{line_num}:{line}', cache[filename][line])
+                new_messages += [f'{filename}:{line_num}:{line}' for line in cache[filename][line]]
                 continue
             messages = []
 
@@ -66,89 +84,125 @@ def lint(filename):
             def warn(start, end, message):
                 messages.append(f'{utf16_column_mapper[start]},{utf16_column_mapper[end]}: WARNING: {message}')
 
-            def yieldChars(chars):
+            def yield_chars(chars, start):
                 s = ''
-                isLeading = True
-                for i, char in enumerate(chars):
-                    if char in non_han:
-                        s += char
-                    else:
+                is_punctuation = True
+                is_leading = True
+                for i, char in enumerate(chars, start):
+                    if (char in non_han) != is_punctuation:
                         if s:
-                            if isLeading:
-                                error(i - len(s), i, "Leading punctuation is not allowed")
-                            elif len(s) > 1:
-                                error(i - len(s), i, "Continuous punctuation is not allowed")
+                            if is_punctuation:
+                                if is_leading:
+                                    error(i - len(s), i, "Leading punctuation or space is not allowed")
+                                elif len(s) > 1:
+                                    error(i - len(s), i, "Continuous punctuation or spaces are not allowed")
+                            else:
+                                yield (i, s)
                             s = ''
-                        isLeading = False
-                        yield (i, char)
+                        is_punctuation = not is_punctuation
+                        is_leading = False
+                    if not is_punctuation and s and not (is_ascii_letter(char) and is_ascii_letter(s[-1])):
+                        yield (i, s)
+                        s = ''
+                    s += char
                 if s:
                     i += 1
-                    error(i - len(s), i, "Trailing punctuation is not allowed")
+                    if is_punctuation:
+                        error(i - len(s), i, "Trailing punctuation or space is not allowed")
+                    else:
+                        yield (i, s)
 
-            def yieldRomans(romans, start):
+            def yield_romans(romans, start):
                 s = ''
-                isSpace = True
-                isLeading = True
+                is_space = True
+                is_leading = True
                 for i, roman in enumerate(romans, start):
-                    if (roman == ' ') != isSpace:
+                    if (roman == ' ') != is_space:
                         if s:
-                            if isSpace:
-                                if isLeading:
+                            if is_space:
+                                if is_leading:
                                     error(i - len(s), i, "Leading space is not allowed")
                                 elif len(s) > 1:
                                     error(i - len(s), i, "Continuous spaces are not allowed")
                             else:
                                 yield (i, s)
                             s = ''
-                        isSpace = not isSpace
-                        isLeading = False
+                        is_space = not is_space
+                        is_leading = False
                     s += roman
                 if s:
                     i += 1
-                    if isSpace:
+                    if is_space:
                         error(i - len(s), i, "Trailing space is not allowed")
                     else:
                         yield (i, s)
 
             utf16_column_mapper = list(accumulate(map(utf16_byte_length, line), initial=1))
             columns = line.rstrip('\n').split(',')
-            if len(columns) < 2:
-                error(0, len(line), 'Invalid line')
-                continue
-            chars, romans, *other_columns = columns
+            columns_start = list(accumulate(map(column_start, columns), initial=0))
+            columns_end = list(accumulate(map(column_start, columns[1:]), initial=len(columns[0])))
 
-            length_mismatch = False
-            for (char_i, char), (roman_i, roman) in zip_longest(yieldChars(chars), yieldRomans(romans, len(chars) + 1), fillvalue=(None, None)):
-                if length_mismatch is not None and (not char or not roman):
-                    length_mismatch = True
-                if char:
-                    if char in multisyllable_allowlist:
-                        length_mismatch = None
-                    if not is_unified_ideograph(char):
-                        error(char_i, char_i + 1, f'Invalid character "{char}"{get_additional_information(char)}')
-                if roman and (char, roman) not in ignoreroman_list:
-                    status = jyutping.validate(roman)
-                    if status == jyutping.ValidationStatus.UNCOMMON:
-                        warn(roman_i - len(roman), roman_i, f'Uncommon jyutping: "{roman}"')
-                    elif status == jyutping.ValidationStatus.INVALID:
-                        error(roman_i - len(roman), roman_i, f'Invalid jyutping: "{roman}"')
+            if chars_column_index is not None and romans_column_index is not None:
+                if chars_column_index < len(columns) > romans_column_index:
+                    length_mismatch = False
+                    for (char_i, char), (roman_i, roman) in zip_longest(
+                        yield_chars(columns[chars_column_index], columns_start[chars_column_index]),
+                        yield_romans(columns[romans_column_index], columns_start[romans_column_index]),
+                        fillvalue=(None, None)
+                    ):
+                        if length_mismatch is not None and (not char or not roman):
+                            length_mismatch = True
+                        if char:
+                            if char in multisyllable_allowlist:
+                                length_mismatch = None
+                            if not is_ascii_letter(char):
+                                if is_simplified_ideograph(char):
+                                    warn(char_i - len(char), char_i, f'Character "{char}" is definitely a simplified ideograph')
+                                elif not is_unified_ideograph(char):
+                                    error(char_i - len(char), char_i, f'Invalid character "{char}"{get_additional_information(char)}')
+                        if roman and (char, roman) not in ignoreroman_list:
+                            status = jyutping.validate(roman)
+                            if status == jyutping.ValidationStatus.UNCOMMON:
+                                warn(roman_i - len(roman), roman_i, f'Uncommon jyutping: "{roman}"')
+                            elif status == jyutping.ValidationStatus.INVALID:
+                                error(roman_i - len(roman), roman_i, f'Invalid jyutping: "{roman}"')
 
-            other_start = len(f'{chars},{romans},')
-            if length_mismatch:
-                warn(0, other_start - 1, 'Word length does not match the number of syllables')
+                    if length_mismatch:
+                        warn(columns_start[start_column_index], columns_end[end_column_index], 'Word length does not match the number of syllables')
+                else:
+                    error(0, columns_end[-1], "Invalid line")
+            else:
+                if chars_column_index is not None:
+                    if chars_column_index < len(columns):
+                        for char_i, char in yield_chars(columns[chars_column_index], columns_start[chars_column_index]):
+                            if not is_ascii_letter(char):
+                                if is_simplified_ideograph(char):
+                                    warn(char_i - len(char), char_i, f'Character "{char}" is definitely a simplified ideograph')
+                                elif not is_unified_ideograph(char):
+                                    error(char_i - len(char), char_i, f'Invalid character "{char}"{get_additional_information(char)}')
+                    else:
+                        error(0, columns_end[-1], "Invalid line")
+                if romans_column_index is not None:
+                    if romans_column_index < len(columns):
+                        for roman_i, roman in yield_romans(columns[romans_column_index], columns_start[romans_column_index]):
+                            status = jyutping.validate(roman)
+                            if status == jyutping.ValidationStatus.UNCOMMON:
+                                warn(roman_i - len(roman), roman_i, f'Uncommon jyutping: "{roman}"')
+                            elif status == jyutping.ValidationStatus.INVALID:
+                                error(roman_i - len(roman), roman_i, f'Invalid jyutping: "{roman}"')
+                    else:
+                        error(0, columns_end[-1], "Invalid line")
 
-            i = other_start
-            for name, value in zip(other_column_names, other_columns):
-                if name in other_column_values and value not in other_column_values[name]:
-                    error(i, i + len(value), f'Illegal value for column "{name}"')
-                i += len(value) + 1
+            for i, (name, value) in enumerate(zip(column_names, columns)):
+                if name in column_values and value not in column_values[name]:
+                    error(columns_start[i], columns_end[i], f'Illegal value for column "{name}"')
 
-            len_other_column_names = len(other_column_names)
-            len_other_columns = len(other_columns)
-            if len_other_column_names != len_other_columns:
-                warn(i - 1 if len_other_columns > len_other_column_names else other_start, len(line), 'Number of columns does not match the header')
+            len_column_names = len(column_names)
+            len_columns = len(columns)
+            if len_column_names != len_columns:
+                warn(columns_end[i], columns_end[-1], 'Number of columns does not match the header')
 
-            new_messages += map(lambda line: f'{filename}:{line_num}:{line}', messages)
+            new_messages += [f'{filename}:{line_num}:{line}' for line in messages]
             cache[filename][line] = messages
 
         curr_messages[filename] = "\n".join(new_messages)
