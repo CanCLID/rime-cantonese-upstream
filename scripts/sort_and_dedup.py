78dac4ae45afd57c445ddcd1952ab3d8bc464a4b
形素叢集・ぐらふぃーむ <graphemecluster@gmail.com>
2023-02-06 01:27:14 +0800

    Improve Scripts (#29)

--- scripts/sort_and_dedup.py
+++ scripts/sort_and_dedup.py
@@ -1,31 +1,26 @@
 from glob import iglob
+from operator import itemgetter, methodcaller
 
-def sort_criteria(line):
-    return tuple(line.split(','))
+def compose(f, g):
+    return lambda arg: f(g(arg))
 
-def variant_sort_criteria(line):
-    columns = line.split(',')
-    return (columns[1], columns[3], columns[0], columns[2])
+sort_cols = {
+    'variant.csv': (1, 3, 0, 2)
+}
 
 for filename in iglob('*.csv'):
     with open(filename, encoding='utf-8') as f:
         header = next(f).rstrip('\n')
-        seen = set()
-        seen_add = seen.add
-        entries = []
-        entries_append = entries.append
-        for line in f:
-            line = line.rstrip('\n')
-            if line not in seen:
-                entries_append(line)
-                seen_add(line)
-
-    if filename == 'variant.csv':
-        entries_sorted = sorted(entries, key=variant_sort_criteria)
-    else:
-        entries_sorted = sorted(entries, key=sort_criteria)
+        sort_criteria = methodcaller('split', ',')
+        if filename in sort_cols:
+            sort_criteria = compose(itemgetter(*sort_cols[filename]), sort_criteria)
+        entries = sorted(f, key=sort_criteria)
 
     with open(filename, 'w', encoding='utf-8') as f:
         print(header, file=f)
-        for line in entries_sorted:
-            print(line, file=f)
+        prev_line = None
+        for line in entries:
+            line = line.rstrip('\n')
+            if line != prev_line:
+                print(line, file=f)
+                prev_line = line
